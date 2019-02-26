60789b423e84a0e3ea58f6322276dee678c19d17
Rishabh Gupta <r.g.gupta@outlook.com>
2019-02-26 20:03:56 +0530

    Revert "simple func"
    This reverts commit d80d62eec7432394913a326d1b6443b839b59691.

--- sample/handler.go
+++ sample/handler.go
@@ -1,6 +1,38 @@
 package function
 
+import (
+	"fmt"
+	"os"
+
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
+)
+
 // Handle a serverless request
 func Handle(req []byte) string {
-	return "Invoked on kinD cluster."
+	config, err := rest.InClusterConfig()
+
+	if err != nil {
+		fmt.Println("couldn't get cluster config - err:", err)
+		os.Exit(-1)
+	}
+
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	data, err := clientset.CoreV1().Namespaces().List(metav1.ListOptions{})
+	if err != nil {
+		panic(err.Error())
+	}
+
+	namespaces := "Namespaces"
+
+	for _, namespace := range data.Items {
+		namespaces = fmt.Sprintf("%s\n%s", namespaces, namespace.GetName())
+	}
+
+	return fmt.Sprintf(namespaces)
 }
